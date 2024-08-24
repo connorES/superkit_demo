d456de17aa60085c82206904f6f5208344c1c1f3
connor <connor.es01@gmail.com>
2024-08-24 17:23:48 +1000

    initial commit

---
+++ app/handlers/auth.go
@@ -0,0 +1,11 @@
+package handlers
+
+import (
+	"superkit_demo/app/types"
+
+	"github.com/anthdm/superkit/kit"
+)
+
+func HandleAuthentication(kit *kit.Kit) (kit.Auth, error) {
+	return types.AuthUser{}, nil
+}
